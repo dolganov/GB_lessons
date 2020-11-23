e8f43e74b858167528865c0d2756d9f81a294bbf
aadolganov <aadolganov@gmail.com>
2020-11-23 09:40:21 -0800

    lesson2 done

--- L2_AntonDolganov.playground/Contents.swift
+++ L2_AntonDolganov.playground/Contents.swift
@@ -12,7 +12,7 @@ func evenNumber(array: [Int]) -> [Int]{
     return realEvenArray
 }
 print(evenNumber(array: [1,2,3,4,5,6,7,8,9,0,11,34,65,87654]))
-print("==================================\n")
+print("================================== 1\n")
 
 //2. Написать функцию, которая определяет, делится ли число без остатка на 3
 
@@ -26,24 +26,31 @@ func divisionByThree(array: [Int]) -> [Int]{
     return realEvenArray
 }
 print(divisionByThree(array: [1,2,3,4,5,6,7,8,9,0,11,34,65,87654]))
-print("==================================\n")
+print("================================== 2\n")
+
+//3. Создать возрастающий массив из 100 чисел.
+var realArray:[Int] = []
+for arr in 1...100 {
+    realArray.append(arr)
+}
+print(realArray)
+print("================================== 3\n")
 
 //4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 
-func delNumber(array: [Int]) -> [Int]{
-    var realEvenArray: [Int] = []
-    var index = 0
-    while index < realEvenArray.count {
-        if realEvenArray[index] % 2 == 0  && realEvenArray[index] % 3 == 0{
-            realEvenArray.remove(at: index)
+
+   var index = 0
+    while index < realArray.count {
+        if realArray[index] % 2 == 0 || realArray[index] % 3 != 0{
+            realArray.remove(at: index)
             index -= 1
         }
         index += 1
     }
-    return realEvenArray
-}
-print(delNumber(array: [1,2,3,4,5,6,7,8,9,0,11,34,789,890,654334,0923,8756,0989,65,87654]))
-print("==================================\n")
+print(realArray)
+print("================================== 4\n")
+
+
 
 // с последним не разобрался
 
